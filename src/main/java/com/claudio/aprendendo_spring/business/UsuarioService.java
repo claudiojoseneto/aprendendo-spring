9ad8ea6b5f35f35a6e739fd935e249569aa44472
Claudio <claudiojoseneto@gmail.com>
2026-08-03 21:18:50 -0300

    Adicionando o projeto completo

---
+++ src/main/java/com/claudio/aprendendo_spring/business/UsuarioService.java
@@ -0,0 +1,51 @@
+package com.claudio.aprendendo_spring.business;
+
+import com.claudio.aprendendo_spring.infrastructure.entity.Usuario;
+import com.claudio.aprendendo_spring.infrastructure.exceptions.ConflictException;
+import com.claudio.aprendendo_spring.infrastructure.exceptions.ResourceNotFoundException;
+import com.claudio.aprendendo_spring.infrastructure.repository.UsuarioRepository;
+import io.jsonwebtoken.security.Password;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public Usuario salvaUsuario(Usuario usuario){
+        try {
+            emailExiste(usuario.getEmail());
+            usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+            return usuarioRepository.save(usuario);
+        } catch (ConflictException e) {
+            throw new ConflictException("Email já cadastrado", e.getCause());
+        }
+    }
+    public void emailExiste(String email){
+        try {
+            boolean existe = vericaEmailExistente(email);
+            if (existe)
+                throw new ConflictException("Email já cadastrado" + email);
+        }catch (ConflictException e){
+                throw new ConflictException("E-mail já cadastrado", e.getCause());
+            }
+    }
+    public boolean vericaEmailExistente (String email){
+        return usuarioRepository.existsByEmail(email);
+    }
+
+    public Usuario buscarUsuarioPorEmail(String email){
+        return usuarioRepository.findByEmail(email).orElseThrow(
+                () -> new ResourceNotFoundException("Email não encontrado" + email));
+    }
+
+    public void deletaUsuarioPorEmail(String email){
+        usuarioRepository.deleteByEmail(email);
+
+    }
+}
---
+++ src/main/java/com/claudio/aprendendo_spring/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.claudio.aprendendo_spring.infrastructure.repository;
+
+import com.claudio.aprendendo_spring.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
