53cf846f01ce19e959c3a6308b6b5e7b64041aa8
Claudio <claudiojoseneto@gmail.com>
2026-08-06 22:31:13 -0300

    fix: remove imports invalidos em Usuario e EnderecoRepository

--- src/main/java/com/claudio/aprendendo_spring/infrastructure/entity/Usuario.java
+++ src/main/java/com/claudio/aprendendo_spring/infrastructure/entity/Usuario.java
@@ -2,7 +2,8 @@ package com.claudio.aprendendo_spring.infrastructure.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.jspecify.annotations.Nullable;
+//import org.jspecify.annotations.Nullable;
+import org.springframework.lang.Nullable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
--- src/main/java/com/claudio/aprendendo_spring/infrastructure/repository/EnderecoRepository.java
+++ src/main/java/com/claudio/aprendendo_spring/infrastructure/repository/EnderecoRepository.java
@@ -1,7 +1,8 @@
 package com.claudio.aprendendo_spring.infrastructure.repository;
 
 import com.claudio.aprendendo_spring.infrastructure.entity.Endereco;
-import org.hibernate.boot.models.JpaAnnotations;
+//import org.hibernate.boot.models.JpaAnnotations;
+//import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
